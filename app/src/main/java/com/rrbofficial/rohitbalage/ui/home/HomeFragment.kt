ca5234137e596b5ac6924ae197f5b1c3b9138986
Rohit Balage <thatcomputerboy26@gmail.com>
2024-09-02 21:17:25 -0400

    added header in home fragment

--- app/src/main/java/com/rrbofficial/rohitbalage/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rrbofficial/rohitbalage/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.rrbofficial.rohitbalage.ui.home
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +8,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.rrbofficial.rohitbalage.R
 import com.rrbofficial.rohitbalage.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +21,28 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        // Start rotation animation for the update text
+        val updateTextView: TextView = binding.root.findViewById(R.id.update_text)
+        startRotationAnimation(updateTextView)
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
         return root
+    }
+
+    private fun startRotationAnimation(view: View) {
+        val animator = ObjectAnimator.ofFloat(view, "rotation", 0f, 360f)
+        animator.duration = 2000 // Animation duration in milliseconds
+        animator.repeatCount = ObjectAnimator.INFINITE // Repeat indefinitely
+        animator.start() // Start the animation
     }
 
     override fun onDestroyView() {
