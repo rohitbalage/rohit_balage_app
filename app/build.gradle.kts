9da5f0d10a436ddc8b80b19c24c3822360c039de
Rohit Balage <thatcomputerboy26@gmail.com>
2024-11-07 14:06:31 -0500

    added icons in app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //material design:
+    implementation(libs.material) // Latest version
 }
